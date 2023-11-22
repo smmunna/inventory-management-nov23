e11f2b655d9478163b507617fb2f4d5707c37254
smmunna <minhazulabedinmunna@gmail.com>
2023-11-22 17:41:55 +0600

    project has been created

---
+++ README.md
@@ -0,0 +1,13 @@
+# Inventory Software
+1. Dashboard
+   - Total Purchase
+   - Total Sales
+   - Total Expense
+   - Total Purchase Due
+   - Total Sales Due
+2. User Management
+   - Add, Delete, Update & Update roles [supplier,customer,admin]
+3. Product
+   - Add Product
+     - Field: id, code, name, 
+
---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+
+function App() {
+
+  return (
+    <div>
+    <h3>Developer Information</h3>
+    <p>
+      <strong>Name:</strong> Minhazul Abedin Munna
+    </p>
+    <p>
+      <strong>Email:</strong> minhazulabedinmunna@gmail.com
+    </p>
+    <p>
+      <strong>Website:</strong> <a href="https://www.techzaint.com">techzaint.com</a>
+    </p>
+    <p>
+      <strong>GitHub:</strong> <a href="https://github.com/smmunna">github.com/smmunna</a>
+    </p>
+    <p>
+      <strong>LinkedIn:</strong>{' '}
+      <a href="https://www.linkedin.com/in/minhazulabedinmunna/">linkedin.com/in/minhazulabedinmunna</a>
+    </p>
+  </div>
+  );
+}
+
+export default App;
